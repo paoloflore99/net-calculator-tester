f4d6931fcac70ef2f86649d821b54c75417af1eb
paolo flore <paoloflore937@gmail.com>
2024-05-09 14:23:35 +0200

    Aggiungere i file di progetto.

---
+++ net-calculator-tester/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_calculator_tester
+{
+    internal class Calculator
+    {
+    }
+}
